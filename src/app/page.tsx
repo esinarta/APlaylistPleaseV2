dbb29ec234b501bee0eaf0d3fc997cb1db6d6f9b
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2023-10-09 12:58:26 -0700

    Add basic search to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,61 @@
+"use client";
+
+import { SearchResults } from "@spotify/web-api-ts-sdk";
+import { useEffect, useState } from "react";
+import sdk from "@/lib/spotify-sdk/ClientInstance";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [searchType, setSearchType] = useState<"artist" | "track">("artist");
+  const [query, setQuery] = useState("");
+  const [results, setResults] =
+    useState<SearchResults<"artist"[] | "track"[]>>();
+
+  useEffect(() => {
+    if (query === "") return;
+
+    (async () => {
+      const results = await sdk.search(query, [searchType]);
+      setResults(() => results);
+    })();
+  }, [query, searchType]);
+
+  return (
+    <div>
+      <label>
+        Artist
+        <input
+          type="radio"
+          name="searchType"
+          value="artist"
+          checked={searchType === "artist"}
+          onChange={(event) => setSearchType("artist")}
+        />
+      </label>
+      <label>
+        Track
+        <input
+          type="radio"
+          name="searchType"
+          value="track"
+          checked={searchType === "track"}
+          onChange={(event) => setSearchType("track")}
+        />
+      </label>
+      <input onChange={(event) => setQuery(event.target.value)} />
+      {results?.artists && (
+        <ul>
+          {results.artists.items.map((artist) => (
+            <li key={artist.id}>{artist.name}</li>
+          ))}
+        </ul>
+      )}
+      {results?.tracks && (
+        <ul>
+          {results.tracks.items.map((track) => (
+            <li key={track.id}>{track.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }

a18eab56df04df6971cf800172f2c046cd48fd23
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2023-10-25 23:54:05 -0700

    Update User component with avatar and dropdown menu

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -4,15 +4,44 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import SpotifyLogo from "../../../public/Spotify_Icon_RGB_Green.png";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const User = () => {
   const { data: session } = useSession();
 
   if (session) {
     return (
-      <div className="flex items-center gap-2">
-        Signed in as {session.user?.name ?? session.user?.email}
-        <Button onClick={() => signOut()}>Sign out</Button>
+      <div className="mr-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <div className="flex items-center gap-2">
+              <Avatar>
+                <AvatarImage src={session.user.image} />
+                <AvatarFallback>
+                  {session.user.name.toUpperCase().charAt(0) ??
+                    session.user.email.toUpperCase().charAt(0)}
+                </AvatarFallback>
+              </Avatar>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-8">
+            <DropdownMenuLabel>
+              Signed in as {session.user?.name ?? session.user?.email}
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => signOut()}>
+              Sign out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     );
   }
